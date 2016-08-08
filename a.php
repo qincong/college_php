74f7b22f09293b182bce22064bb6419466c71815
ubuntu <qin_cong@foxmail.com>
2016-08-08 22:14:24 +0800

    为Android读课程表作了适配

--- a.php
+++ a.php
@@ -3,6 +3,9 @@
     function a() {
         echo "a";
     }//qincong
+    function b($str) {
+        echo $str;
+    }
     $json='{"a":1,"b":2}';
     $obj=json_decode($json);
     echo $obj->{"a"};
--- getlogin.php
+++ getlogin.php
@@ -1,6 +1,11 @@
 <?php
 //??????
 {
+    require_once ("get_class.php");
+    $student_number = $_GET["number"];
+    $student_name = $_GET["name"];
+    $password = $_GET["password"];
+    $checkcode = $_GET["checkcode"];
     $cookie_file = dirname(__FILE__)."/".'cookie.txt';
     $login_url = "http://jwgl.ujn.edu.cn/default2.aspx";
 
@@ -12,22 +17,24 @@
     $post_data = array(
         "Button1" => "",
         "RadioButtonList1" =>iconv( "UTF-8","GB2312//IGNORE", "学生"),
-        "TextBox2" =>"zhzh1995.",
+        "TextBox2" =>$password,
         "__VIEWSTATE" => "dDwyODE2NTM0OTg7Oz7SB83rscHSkDngMd3SzAQW8HUNsA==",
         "__VIEWSTATEGENERATOR"=>"92719903",
         "hidPdrs" =>"",
         "hidsc"=>"",
         "lbLanguage"=>"",
-        "txtSecretCode"=>"brt6",  //?????
-        "txtUserName"=>"20141223251"
+        "txtSecretCode"=>$checkcode,
+        "txtUserName"=>$student_number
     );
     //aecho $post_data["txtSecretCode"];
     //echo $post_data["txtSecretCode"];
     curl_setopt($cu_3, CURLOPT_POSTFIELDS, $post_data);
     $data = curl_exec($cu_3);
-    curl_setopt($cu_3,CURLOPT_URL,"http://jwgl.ujn.edu.cn/xs_main.aspx?xh=20141223251");
+    curl_setopt($cu_3,CURLOPT_URL,"http://jwgl.ujn.edu.cn/xs_main.aspx?xh=".$student_number);
     $data = curl_exec($cu_3);
-    echo $data;
+    get_classtable($student_number,$student_name);
+    //echo $data;
+    //echo $student_name;
 }
 
 function read()
